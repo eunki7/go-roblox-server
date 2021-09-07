54785afeafa2276c14a018c347856053b647eb12
eunki7 <outsider7224@hanmail.net>
2021-09-07 13:24:42 +0900

    modify db.ini

--- docs/sql/blog.sql
+++ docs/sql/blog.sql
@@ -7,7 +7,6 @@ Target Server Type    : MYSQL
 Target Server Version : 50639
 File Encoding         : 65001
 
-Date: 2018-03-18 16:52:35
 */
 
 SET FOREIGN_KEY_CHECKS=0;
@@ -18,19 +17,19 @@ SET FOREIGN_KEY_CHECKS=0;
 DROP TABLE IF EXISTS `blog_article`;
 CREATE TABLE `blog_article` (
   `id` int(10) unsigned NOT NULL AUTO_INCREMENT,
-  `tag_id` int(10) unsigned DEFAULT '0' COMMENT '标签ID',
-  `title` varchar(100) DEFAULT '' COMMENT '文章标题',
-  `desc` varchar(255) DEFAULT '' COMMENT '简述',
+  `tag_id` int(10) unsigned DEFAULT '0' COMMENT 'ID',
+  `title` varchar(100) DEFAULT '' COMMENT 'TITLE',
+  `desc` varchar(255) DEFAULT '' COMMENT 'DESCRIPTION',
   `content` text COMMENT '内容',
-  `cover_image_url` varchar(255) DEFAULT '' COMMENT '封面图片地址',
-  `created_on` int(10) unsigned DEFAULT '0' COMMENT '新建时间',
-  `created_by` varchar(100) DEFAULT '' COMMENT '创建人',
-  `modified_on` int(10) unsigned DEFAULT '0' COMMENT '修改时间',
-  `modified_by` varchar(255) DEFAULT '' COMMENT '修改人',
+  `cover_image_url` varchar(255) DEFAULT '' COMMENT 'COVER IMAGE',
+  `created_on` int(10) unsigned DEFAULT '0' COMMENT 'CREATE DATE',
+  `created_by` varchar(100) DEFAULT '' COMMENT 'CREATOR',
+  `modified_on` int(10) unsigned DEFAULT '0' COMMENT 'MODIFY DATE',
+  `modified_by` varchar(255) DEFAULT '' COMMENT 'MODIFIER',
   `deleted_on` int(10) unsigned DEFAULT '0',
-  `state` tinyint(3) unsigned DEFAULT '1' COMMENT '删除时间',
+  `state` tinyint(3) unsigned DEFAULT '1' COMMENT 'STATE',
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8 COMMENT='文章管理';
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COMMENT='ENGINE';
 
 -- ----------------------------
 -- Table structure for blog_auth
@@ -38,8 +37,8 @@ CREATE TABLE `blog_article` (
 DROP TABLE IF EXISTS `blog_auth`;
 CREATE TABLE `blog_auth` (
   `id` int(10) unsigned NOT NULL AUTO_INCREMENT,
-  `username` varchar(50) DEFAULT '' COMMENT '账号',
-  `password` varchar(50) DEFAULT '' COMMENT '密码',
+  `username` varchar(50) DEFAULT '' COMMENT 'USER NAME',
+  `password` varchar(50) DEFAULT '' COMMENT 'PASSWORD',
   PRIMARY KEY (`id`)
 ) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8;
 
@@ -51,13 +50,13 @@ INSERT INTO `blog_auth` (`id`, `username`, `password`) VALUES ('1', 'test', 'tes
 DROP TABLE IF EXISTS `blog_tag`;
 CREATE TABLE `blog_tag` (
   `id` int(10) unsigned NOT NULL AUTO_INCREMENT,
-  `name` varchar(100) DEFAULT '' COMMENT '标签名称',
-  `created_on` int(10) unsigned DEFAULT '0' COMMENT '创建时间',
-  `created_by` varchar(100) DEFAULT '' COMMENT '创建人',
-  `modified_on` int(10) unsigned DEFAULT '0' COMMENT '修改时间',
-  `modified_by` varchar(100) DEFAULT '' COMMENT '修改人',
-  `deleted_on` int(10) unsigned DEFAULT '0' COMMENT '删除时间',
-  `state` tinyint(3) unsigned DEFAULT '1' COMMENT '状态 0为禁用、1为启用',
+  `name` varchar(100) DEFAULT '' COMMENT 'USER NAME',
+  `created_on` int(10) unsigned DEFAULT '0' COMMENT 'CREATE DATE',
+  `created_by` varchar(100) DEFAULT '' COMMENT 'CREATOR',
+  `modified_on` int(10) unsigned DEFAULT '0' COMMENT 'MODIFY DATE',
+  `modified_by` varchar(100) DEFAULT '' COMMENT 'MODIFIER',
+  `deleted_on` int(10) unsigned DEFAULT '0' COMMENT 'DELETED',
+  `state` tinyint(3) unsigned DEFAULT '1' COMMENT 'STATE',
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8 COMMENT='文章标签管理';
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COMMENT='ENGINE';
 
